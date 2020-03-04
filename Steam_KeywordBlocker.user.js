d433eb1e12604f8b5caac27de70669f416bf845b
CYTMWIA <cytmwia@gmail.com>
2020-03-05 01:58:36 +0800

    修复Bug：在Steam首页清除第一个社区推荐后，第一次的“下一个”需要点击两次；添加对游戏商店页面评测的支持

--- Steam_KeywordBlocker.user.js
+++ Steam_KeywordBlocker.user.js
@@ -5,6 +5,7 @@
 // @author       CYTMWIA
 // @match        http*://store.steampowered.com/
 // @match        http*://store.steampowered.com/labs/trendingreviews/*
+// @match        http*://store.steampowered.com/app/*
 // @run-at       document-body
 // ==/UserScript==
 
@@ -23,12 +24,19 @@
         return false
     }
 
-    if (window.location.pathname=='/') {
+    function setIntervalKiller(func,timeout,self_clear=false) {
         let interval_id = setInterval(()=>{
+            if (func()&&self_clear) {
+                clearInterval(interval_id)
+            }
+        },timeout)
+        return interval_id
+    }
+
+    if (window.location.pathname=='/') {
+        setIntervalKiller(()=>{
             let apps = document.getElementsByClassName('community_recommendation_app')
             if (apps.length>0){
-                clearInterval(interval_id)
-
                 let thumbs = document.getElementsByClassName('carousel_thumbs')[3]
                 let focus = true
                 for (let idx=apps.length-1;idx>=0;idx-=1){
@@ -42,14 +50,15 @@
                     }
                 }
                 if (!focus&&apps.length>0) {
-                    apps[0].parentElement.className = 'focus'
-                    thumbs.children[0].className = 'focus'
+                    document.getElementsByClassName('arrow right')[3].click()
                 }
+
+                return true
             }
 
-        },500);
+        },500,true);
     } else if (window.location.pathname.includes('/labs/trendingreviews')) {
-        setInterval(()=>{
+        setIntervalKiller(()=>{
             let apps = document.getElementById('reviewed_apps').children
             for (let idx=apps.length-1;idx>=0;idx-=1) {
                 if (containWordInList(apps[idx].innerHTML)) {
@@ -57,5 +66,14 @@
                 }
             }
         },500)
+    } else if (window.location.pathname.indexOf('/app')==0) {
+        setIntervalKiller(()=>{
+            let reviews = document.getElementsByClassName('review_box')
+            for (let idx=reviews.length-1;idx>=0;idx-=1) {
+                if (containWordInList(reviews[idx].innerHTML)) {
+                    reviews[idx].remove()
+                }
+            }
+        },500,true)
     }
 })();
